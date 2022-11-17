4f4922bb6921fcaa9aad635d1c64dfa9d9184246
Baobab <baobab.ingenierie@outlook.com>
2022-11-17 11:03:04 +0100

    CE999 Day 4

---
+++ js/init-bases.js
@@ -0,0 +1,28 @@
+// Commentaire sur une ligne
+// puis sur une autre ligne
+
+/*
+Commentaire
+sur
+plusieurs
+lignes
+*/
+
+/**
+ * Variables primitives : string, number et boolean
+ */
+var nb = 7;
+var nb = "sept"; // JS faiblement typé !
+
+var distanciel = 4; // Déclaration explicite
+var presentiel = 3;
+
+// Exploitation des variables et constantes
+var code = 'CE999'; // Type String
+var titre = "Langages HTML5, CSS3 et JS";
+var duree = 5; // Type Number
+var isDev = true; // Type Boolean
+const public = 7; // Constante
+// public = distanciel + presentiel;
+console.log('Hello World!'); // Affiche un message dans la console
+console.log(titre);
